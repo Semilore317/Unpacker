862611456f380f9c4245c02b569fe6ee5a9d8af0
Abraham Bankole <semi.banks.317@gmail.com>
2026-01-08 12:56:38 +0100

    updated User Interface to reflect the appinfo section

---
+++ Unpacker/AppInfoCard.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Unpacker;
+
+public partial class AppInfoCard : UserControl
+{
+    public AppInfoCard()
+    {
+        InitializeComponent();
+    }
+
+    // Allow parent to bind: <local:AppInfoCard AppName="{Binding Name}" />
+    public static readonly StyledProperty<string> AppNameProperty =
+        AvaloniaProperty.Register<AppInfoCard, string>(nameof(AppName), "Application Name");
+
+    public string AppName
+    {
+        get => GetValue(AppNameProperty);
+        set => SetValue(AppNameProperty, value);
+    }
+
+    public static readonly StyledProperty<string> AppVersionProperty =
+        AvaloniaProperty.Register<AppInfoCard, string>(nameof(AppVersion), "1.0.0");
+
+    public string AppVersion
+    {
+        get => GetValue(AppVersionProperty);
+        set => SetValue(AppVersionProperty, value);
+    }
+}
